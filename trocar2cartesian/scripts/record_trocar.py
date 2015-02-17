da4d7eb7b95847e434f44fb6a125cd50f9d44d71
ahb <andreas.bihlmaier@gmx.de>
2015-02-17 18:25:39 +0100

    Record trocar position based on instrument/endoscope tip, e.g. rosrun trocar2cartesian record_trocar.py or_table_with_lwr1_with_wolf_endocam_hd_with_wolf_panoview_0deg__lwr_1_with_wolf_endocam_hd_with_wolf_panoview_0deg__lwr_1_with_marker_and_coupling__lwr_1_socket__lwr1__lwr1_world_link or_table_with_lwr1_with_wolf_endocam_hd_with_wolf_panoview_0deg__lwr_1_with_wolf_endocam_hd_with_wolf_panoview_0deg__wolf_endocam_hd_complete_with_wolf_panoview_0deg__wolf_panoview_0deg__optical_link

---
+++ trocar2cartesian/scripts/record_trocar.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+import sys
+import argparse
+import tf
+import numpy as np
+from tf.transformations import *
+from geometry_msgs.msg import Pose
+from trocar2cartesian_msgs.srv import SetTrocarRequest
+
+
+
+def translation_quaternion2pose_msg(translation, quaternion):
+  pose = Pose()
+  pose.position.x = translation[0]
+  pose.position.y = translation[1]
+  pose.position.z = translation[2]
+  pose.orientation.x = quaternion[0]
+  pose.orientation.y = quaternion[1]
+  pose.orientation.z = quaternion[2]
+  pose.orientation.w = quaternion[3]
+  return pose
+
+
+
+def main(args):
+  parser = argparse.ArgumentParser()
+  parser.add_argument('robot_base_tf', help='Robot Base Frame')
+  parser.add_argument('instrument_tip_tf', help='Instrument Tip Frame')
+  args = parser.parse_args(rospy.myargv()[1:])
+  robot_base_tf, instrument_tip_tf = args.robot_base_tf, args.instrument_tip_tf
+
+  rospy.init_node('record_trocar', anonymous=True)
+  tf_listener = tf.TransformListener()
+
+  tf_listener.waitForTransform(robot_base_tf, instrument_tip_tf, rospy.Time(), rospy.Duration(4.0))
+  position, quaternion = tf_listener.lookupTransform(robot_base_tf, instrument_tip_tf, rospy.Time())
+  set_trocar_msg = SetTrocarRequest()
+  set_trocar_msg.trocar_frame = robot_base_tf
+  set_trocar_msg.instrument_tip_frame = instrument_tip_tf
+  set_trocar_msg.trocar_pose = translation_quaternion2pose_msg(position, quaternion)
+  print('Now move robot into trocar and afterwards:')
+  print("rosservice call /robots/lwr1/set_trocar '\n%s\n'" % set_trocar_msg)
+
+
+if __name__ == '__main__':
+  main(sys.argv)
